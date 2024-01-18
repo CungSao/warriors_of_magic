bbd72833394f9762e80803b173fbdb657bf39ec7
CungSao <2022bevy@gmail.com>
2024-01-18 14:25:06 +0700

    done healingShop

--- healingShop.php
+++ healingShop.php
@@ -23,17 +23,54 @@ function healingShop() {
         ),
     );
 
-    // Chưa buy
+    if(!empty($_POST)) {
+        foreach ($healing as $name => $kit) {
+            if (!empty($_POST[$name])) {
+                try {
+                    if ($player->money < $kit['cost']) {
+                        throw new Exception("You don't have enough money to buy this health kit.");
+                    }
+                    elseif($player->health >= 100) {
+                        throw new Exception("You already have full health.");
+                    }
+                    
+                    // Purchase
+                    $player->money -= $kit['cost'];
+                    $player->health += $kit['health'];
+                    $player->update();
+                    echo "<p>You have purchased a {$name} health kit.<br>
+                        Your health is now {$player->health}.<br>
+                        You still have {$player->money}$</p>";
+    
+                } catch (Exception $e) {
+                    echo "<p>{$e->getMessage()}</p>";
+                }
+            }
+        }
+    }
 
     // Display
     $label_width = 20;
-    echo "<div class='formContainer centerDiv' style='text-align:left;width: 400px;'
-        <form action='$self_link' method='POST'>";
-            foreach ($healing as $name => $kit) {
-                echo "<label style='width:{$label_width}em;'>" . ucwords($name) . " health kit (\${$kit['cost']}, {$kit['health']} health)</label>
-                    <input type='submit' name='$name' value='Purchase'><br>";
-            }
-        echo "</form>
-    </div>";
+    echo "<table class='formContainer centerDiv' style='width:500px;'>
+        <tr>
+            <th style='width:{$label_width}%;'>Name</th>
+            <th style='width:{$label_width}%;'>Health</th>
+            <th style='width:{$label_width}%;'>Price</th>
+            <th style='width:{$label_width}%;'>&nbsp;</th>
+        </tr>";
+        foreach ($healing as $name => $kit) {
+            echo "<tr>
+                <td>" . ucwords($name) . " health kit</td>
+                <td>{$kit['health']}</td>
+                <td>{$kit['cost']}</td>
+                <td>
+                    <form action='$self_link' method='POST'>
+                        <br><input  type='submit' name='$name' value='Purchase'>
+                    </form>
+                </td>
+            </tr>";
+        }
+    echo "</table>";
 }
+
 ?>
